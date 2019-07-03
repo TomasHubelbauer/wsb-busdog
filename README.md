bed907e3114ce3649a20d0feedb03c0b63468225
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-07-03 20:21:29 +0200

    Attempt at running USBDog in Windows Sandbox

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # USBDog Windows Sandbox Launcher
 
 This repository contains a Windows Sandbox Launcher which downloads and runs USBDog.
+
+USBDog doesn't seem to be able to run in Windows Sandbox.
---
+++ boot.bat
@@ -0,0 +1,2 @@
+REM https://github.com/djpnewton/busdog
+bitsadmin /transfer usbdog /download https://github.com/djpnewton/busdog/blob/master/builds/0.2.2/busdog_signed_0.2.2.zip C:\Users\WDAGUtilityAccount\Desktop\busdog_signed_0.2.2.zip
